f5de84e2c6430acf090570702e749cca4f747e83
Matteo <matteo.3000@outlook.it>
2024-04-27 23:07:20 +0200

    lavoro finito. aggiunte grafiche e implementazione admin su news

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Web.Mvc;
 using VolleyVerse.Models;
 
@@ -8,7 +9,71 @@ public class NewsController : Controller
 
     public ActionResult Index()
     {
-        var sortedNews = db.Notizie.OrderByDescending(n => n.date_published).ToList();
-        return View(sortedNews);
+        var news = db.Notizie.OrderByDescending(n => n.date_published).ToList();
+        return View(news);
+    }
+
+    public ActionResult Create()
+    {
+        if (Request.Cookies["AdminIDCookie"] == null)
+            return RedirectToAction("Index");
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Notizie news)
+    {
+        if (Request.Cookies["AdminIDCookie"] == null)
+            return RedirectToAction("Index");
+
+        if (ModelState.IsValid)
+        {
+            db.Notizie.Add(news);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(news);
+    }
+
+    public ActionResult Edit(int? id)
+    {
+        if (Request.Cookies["AdminIDCookie"] == null || id == null)
+            return RedirectToAction("Index");
+
+        var news = db.Notizie.Find(id);
+        if (news == null)
+            return HttpNotFound();
+
+        return View(news);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(Notizie news)
+    {
+        if (Request.Cookies["AdminIDCookie"] == null)
+            return RedirectToAction("Index");
+
+        if (ModelState.IsValid)
+        {
+            db.Entry(news).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(news);
+    }
+
+    public ActionResult Delete(int? id)
+    {
+        if (Request.Cookies["AdminIDCookie"] == null || id == null)
+            return RedirectToAction("Index");
+
+        var news = db.Notizie.Find(id);
+        if (news == null)
+            return HttpNotFound();
+        db.Notizie.Remove(news);
+        db.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
